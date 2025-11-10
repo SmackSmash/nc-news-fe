ceeeb96fdacbeacaff758626bf8a8d60a6bd2a22
Dan Moffat <danmoffat@gmail.com>
2025-11-10 15:47:13 +0000

    Added a nice button

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,7 +1,12 @@
+import Button from '../Button/Button';
 import './SideBar.css';
 
 const SideBar = () => {
-  return <aside id='sideBar'>SideBar works!</aside>;
+  return (
+    <aside id='sideBar'>
+      <Button>+ Add Topic</Button>
+    </aside>
+  );
 };
 
 export default SideBar;
