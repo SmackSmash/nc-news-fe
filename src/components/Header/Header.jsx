7c60bbf18b41001be6b9edd5c88b629c43b9500f
Dan Moffat <danmoffat@gmail.com>
2025-11-10 14:26:18 +0000

    Fleshed out header and basic styles

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,14 @@
+import Logo from '../Logo/Logo';
+import NavBar from '../NavBar/NavBar';
 import './Header.css';
 
 const Header = () => {
-  return <h1>Header Works!</h1>;
+  return (
+    <header id='header' className='container'>
+      <Logo />
+      <NavBar />
+    </header>
+  );
 };
 
 export default Header;
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,7 @@
+import './NavBar.css';
+
+const NavBar = () => {
+  return <h1>NavBar works!</h1>;
+};
+
+export default NavBar;
