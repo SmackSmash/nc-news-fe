bd80f0861e8f363725f9c0cf6b9b5ec26c9ee43e
Dan Moffat <danmoffat@gmail.com>
2025-11-10 16:19:25 +0000

    Added custom pill colors

--- src/components/PillLink/PillLink.jsx
+++ src/components/PillLink/PillLink.jsx
@@ -1,7 +1,18 @@
+import classNames from 'classnames';
 import './PillLink.css';
 
 const PillLink = ({ href, color, margin, children }) => {
-  return <a href={href}>{children}</a>;
+  const btnClass = classNames({
+    pill: true,
+    [color]: color,
+    margin: margin
+  });
+
+  return (
+    <a href={href} className={btnClass}>
+      {children}
+    </a>
+  );
 };
 
 export default PillLink;
