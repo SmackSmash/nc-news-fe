9e695a40fd4813e1f4808a33713f941fab6a84f4
Dan Moffat <danmoffat@gmail.com>
2025-11-13 20:21:51 +0000

    Fixed bug in formatDate function

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -12,15 +12,15 @@ const formatDate = date => {
   } else if (days > 1) {
     return `${days} days ago`;
   } else if (days === 1) {
-    `${days} day ago`;
+    return `${days} day ago`;
   } else if (hours > 1) {
     return `${hours} hours ago`;
   } else if (hours === 1) {
-    `${hours} hour ago`;
+    return `${hours} hour ago`;
   } else if (minutes > 1) {
     return `${minutes} minutes ago`;
   } else if (minutes === 1) {
-    `${minutes} minute ago`;
+    return `${minutes} minute ago`;
   } else if (seconds > 1) {
     return `${seconds} seconds ago`;
   } else {
