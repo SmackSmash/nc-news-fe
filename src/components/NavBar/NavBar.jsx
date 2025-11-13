bc2a672fd4a5fdcba33e9c13aa7d7aa8eb180a8f
Dan Moffat <danmoffat@gmail.com>
2025-11-13 12:21:07 +0000

    Added hover effects for nav links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,13 +4,13 @@ import './NavBar.css';
 const NavBar = ({ topicData }) => {
   return (
     <nav id='nav'>
+      <NavLink to='/'>Articles</NavLink>
       {topicData &&
         topicData.topics.map(({ slug }) => (
           <NavLink to={`/topic/${slug}`} className='topic'>
             {slug[0].toUpperCase() + slug.slice(1)}
           </NavLink>
         ))}
-      <NavLink to='/'>Articles</NavLink>
       <NavLink to='/users'>Users</NavLink>
     </nav>
   );
