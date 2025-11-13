39714e11cc19d680a852f73a9643e1926dbcf493
Dan Moffat <danmoffat@gmail.com>
2025-11-13 14:34:05 +0000

    Implemented basic mobile menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,18 +1,32 @@
 import { NavLink } from 'react-router';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import './NavBar.css';
+import { useState } from 'react';
 
 const NavBar = ({ topicData }) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
-    <nav id='nav'>
-      <NavLink to='/'>Articles</NavLink>
-      {topicData &&
-        topicData.topics.map(({ slug }) => (
-          <NavLink to={`/topic/${slug}`} className='topic'>
-            {slug[0].toUpperCase() + slug.slice(1)}
-          </NavLink>
-        ))}
-      <NavLink to='/users'>Users</NavLink>
-    </nav>
+    <>
+      <button id='mobileMenu'>
+        <GiHamburgerMenu onClick={() => setIsMenuOpen(!isMenuOpen)} />
+      </button>
+      <div onClick={() => setIsMenuOpen(false)} id='mobileOverlay' className={isMenuOpen && 'open'}></div>
+      <nav id='nav' className={isMenuOpen && 'open'}>
+        <NavLink onClick={() => setIsMenuOpen(false)} to='/'>
+          Articles
+        </NavLink>
+        {topicData &&
+          topicData.topics.map(({ slug }) => (
+            <NavLink onClick={() => setIsMenuOpen(false)} to={`/topic/${slug}`} className='topic'>
+              {slug[0].toUpperCase() + slug.slice(1)}
+            </NavLink>
+          ))}
+        <NavLink onClick={() => setIsMenuOpen(false)} to='/users'>
+          Users
+        </NavLink>
+      </nav>
+    </>
   );
 };
 
