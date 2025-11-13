f45e31d614ea69b2017fc1dc6eb5307154d93885
Dan Moffat <danmoffat@gmail.com>
2025-11-13 20:49:06 +0000

    Added weeks to formatDate function

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -6,9 +6,14 @@ const formatDate = date => {
   const minutes = Math.floor(seconds / 60);
   const hours = Math.floor(minutes / 60);
   const days = Math.floor(hours / 24);
+  const weeks = Math.floor(days / 7);
 
-  if (days > 30) {
+  if (weeks > 4) {
     return then.toDateString();
+  } else if (weeks > 1) {
+    return `${days} weeks ago`;
+  } else if (weeks === 1) {
+    return `${weeks} weeks ago`;
   } else if (days > 1) {
     return `${days} days ago`;
   } else if (days === 1) {
