077532f0a9354d6ed5246f7271a01829ec981035
Dan Moffat <danmoffat@gmail.com>
2025-11-13 20:16:11 +0000

    Even nicer date formatting

--- src/utils/formatDate.js
+++ src/utils/formatDate.js
@@ -8,7 +8,7 @@ const formatDate = date => {
   const days = Math.floor(hours / 24);
 
   if (days > 30) {
-    return then.toLocaleDateString();
+    return then.toDateString();
   } else if (days > 1) {
     return `${days} days ago`;
   } else if (days === 1) {
