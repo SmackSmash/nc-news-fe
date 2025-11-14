4e9139dfefd2037acf97141d0dd2b07ac71a804b
Dan Moffat <danmoffat@gmail.com>
2025-11-14 13:52:24 +0000

    Added error handling for likes, even with optimistic rendering

--- src/components/LikeArticle/LikeArticle.jsx
+++ src/components/LikeArticle/LikeArticle.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { FaThumbsUp, FaRegThumbsUp, FaThumbsDown, FaRegThumbsDown } from 'react-icons/fa';
+import Error from '../Error/Error';
 import './LikeArticle.css';
 
 const LikeArticle = ({ articleId, votes }) => {
@@ -7,45 +8,59 @@ const LikeArticle = ({ articleId, votes }) => {
   const [touched, setTouched] = useState(false);
   const [likeTouched, setLikeTouched] = useState(false);
   const [dislikeTouched, setDislikeTouched] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleLike = async () => {
     setTouched(true);
     setLikeTouched(true);
     setLikes(likes + 1);
-    await fetch(`https://northcoders-news-be-f4oe.onrender.com/api/articles/${articleId}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ inc_votes: 1 })
-    });
+    try {
+      await fetch(`https://northcoders-news-be-f4oe.onrender.com/api/articles/${articleId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ inc_votes: 1 })
+      });
+    } catch (error) {
+      setLikes(likes => likes - 1);
+      setError(error);
+    }
   };
 
   const handleDislike = async () => {
     setTouched(true);
     setDislikeTouched(true);
     setLikes(likes - 1);
-    await fetch(`https://northcoders-news-be-f4oe.onrender.com/api/articles/${articleId}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify({ inc_votes: -1 })
-    });
+    try {
+      await fetch(`https://northcoders-news-be-f4oe.onrender.com/api/articles/${articleId}`, {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ inc_votes: -1 })
+      });
+    } catch (error) {
+      setLikes(likes => likes + 1);
+      setError(error);
+    }
   };
 
   return (
-    <div id='likeArticle'>
-      <button onClick={handleLike} disabled={touched} className={likeTouched ? 'likeTouched' : ''}>
-        {likeTouched ? <FaThumbsUp /> : <FaRegThumbsUp />}
-      </button>
-      <span>
-        {likes} like{likes !== 1 && 's'}
-      </span>
-      <button onClick={handleDislike} disabled={touched} className={dislikeTouched ? 'dislikeTouched' : ''}>
-        {dislikeTouched ? <FaThumbsDown /> : <FaRegThumbsDown />}
-      </button>
-    </div>
+    <>
+      <div id='likeArticle'>
+        <button onClick={handleLike} disabled={touched} className={likeTouched ? 'likeTouched' : ''}>
+          {likeTouched ? <FaThumbsUp /> : <FaRegThumbsUp />}
+        </button>
+        <span>
+          {likes} like{likes !== 1 && 's'}
+        </span>
+        <button onClick={handleDislike} disabled={touched} className={dislikeTouched ? 'dislikeTouched' : ''}>
+          {dislikeTouched ? <FaThumbsDown /> : <FaRegThumbsDown />}
+        </button>
+      </div>
+      {error && <p id='likeError'>Something went wrong.</p>}
+    </>
   );
 };
 
