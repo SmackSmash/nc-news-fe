0e70207b69f11c90a9832e92ff6c97562a6683b8
Dan Moffat <danmoffat@gmail.com>
2025-11-14 17:31:46 +0000

    Added fade in for image component

--- src/components/Image/Image.jsx
+++ src/components/Image/Image.jsx
@@ -1,7 +1,16 @@
+import { useState } from 'react';
+import classNames from 'classnames';
 import './Image.css';
 
 const Image = ({ src, alt }) => {
-  return <img src={src} alt={alt} />;
+  const [loaded, setLoaded] = useState(false);
+
+  const imgClasses = classNames({
+    fadeImage: true,
+    loaded
+  });
+
+  return <img onLoad={() => setLoaded(true)} src={src} alt={alt} className={imgClasses} />;
 };
 
 export default Image;
